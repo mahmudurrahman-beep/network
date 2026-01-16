564dd2bcf99a3211167c285df640ab4991dc2ade
Tushar_github <mahmudurrahman23yahoo@gmail.com>
2026-01-16 18:09:17 +0600

    Update views.py

--- network/views.py
+++ network/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.core.paginator import Paginator
 from django.views.decorators.csrf import csrf_exempt
