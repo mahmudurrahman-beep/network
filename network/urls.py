231a1aa5db8191f4953e5ed51dd82612f3b9d333
Tushar_github <mahmudurrahman23yahoo@gmail.com>
2026-01-16 15:24:36 +0600

    Update urls.py

--- network/urls.py
+++ network/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.index, name="index"),
