f0eee7f8848ff5ee7f8a5fe761ad53384e920807
Tushar_github <mahmudurrahman23yahoo@gmail.com>
2026-01-16 21:21:47 +0600

    Update urls.py

--- network/urls.py
+++ network/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
-app_name = 'network'
 
 urlpatterns = [
     # Main pages
